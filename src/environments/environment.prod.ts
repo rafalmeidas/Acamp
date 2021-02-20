2c9f1ee4e8a69e0d5aa0b2b057405e4049f17dc6
halefalmeida <halef.dorigan@outlook.com>
2021-02-20 10:11:19 -0300

    add firebase app configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  API_URL: 'http://18.230.23.24:3000/'
+  API_URL: 'http://18.230.23.24:3000/',
+  //Configuração Firebase
+  firebaseConfig: {
+    apiKey: "AIzaSyAdv-5_4sRAnOo6oKhlv5Qln80TGDlBxXQ",
+    authDomain: "acamp-brasil.firebaseapp.com",
+    projectId: "acamp-brasil",
+    storageBucket: "acamp-brasil.appspot.com",
+    messagingSenderId: "9503616269",
+    appId: "1:9503616269:web:22fe480f4089c8259c1444",
+    measurementId: "G-YCL9EYK8GS"
+  }
 };
